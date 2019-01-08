2724116472d45855fc71ff938ce01f0facc379fd
wujide <wujide2015@126.com>
2019-01-08 23:12:08 +0800

    debug

--- demo/dup_file_md5.py
+++ demo/dup_file_md5.py
@@ -1,8 +1,16 @@
 import hashlib
+import os
+import shutil
+import time
 from pathlib import Path
 
 dup = {}
-photo_path = 'E:\\test_files'
+PHOTO_PATH = '/Users/wujide/Documents/照片备份/'
+DUP_FILE_PATH = '/Users/wujide/Documents/duplicated_files/'
+
+
+def move_files(duplicate_files):
+    return shutil.move(duplicate_files, DUP_FILE_PATH)
 
 
 def md5sum(filename, blocksize=65536):
@@ -30,9 +38,19 @@ def main():
     def get_duplicate():
         return {k: v for k, v in dup.items() if len(v) > 1}
 
-    build_dup_dict(photo_path)
+    build_dup_dict(PHOTO_PATH)
     for hash, files in get_duplicate().items():
         print("{}: {}".format(hash, files))
+        print("moving file===: ", files[1], "==> to [dup_file_path]")
+        for file in files[1:]:
+            try:
+                move_files(file)
+            except shutil.Error:
+                print("Destination path(files) already exists!")
+                print("rename the exists files: ", file)
+                file_rename = file + time.strftime('%Y%m%d %H%M%S')
+                os.renames(file, file_rename)
+                move_files(file_rename)
 
 
 if __name__ == '__main__':
