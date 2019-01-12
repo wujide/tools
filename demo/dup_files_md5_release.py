8431884099f73938ea8cf461adc1ec7c81cfefa1
wujide <wujide2015@126.com>
2019-01-12 21:55:54 +0800

    add comments

--- demo/dup_files_md5_release.py
+++ demo/dup_files_md5_release.py
@@ -9,8 +9,8 @@ from demo.log_redirection import Logger
 # sys.stdout = Logger('dup_file_log.txt')
 
 dup = {}
-dir_num = 0
-file_num = 0
+dir_num = 0  # 文件夹个数
+file_num = 0  # 文件个数
 
 """
 1. 支持文件名不同但实际是同一文件的去重
@@ -23,19 +23,22 @@ file_num = 0
 """
 
 
+# 将 src 文件移入 dest_path 路径下
 def move_files(src, dest_path):
-    # os.chmod(dest_path, stat.S_IWOTH)  # 更改文件属性，使其它用户具有写权限
+    # os.chmod(dest_path, stat.S_IWOTH)  # 更改文件属性，使其它用户具有写权限，windows需要
     return shutil.move(src, dest_path)
 
 
+# 计算文件的md5
 def md5sum(filename, blocksize=65536):
-    hash_handle = hashlib.md5()
+    md5 = hashlib.md5()
     with open(filename, "rb") as f:
         for block in iter(lambda: f.read(blocksize), b""):
-            hash_handle.update(block)
-    return hash_handle.hexdigest()
+            md5.update(block)
+    return md5.hexdigest()
 
 
+# 建立文件的md5:文件路径的对应关系，多个相同文件则为md5:[文件列表]
 def build_dup_dict(dir_path, pattern='*.*'):
     def save(file):
         hash_val = md5sum(file)
@@ -49,6 +52,7 @@ def build_dup_dict(dir_path, pattern='*.*'):
             save(str(item))
 
 
+# 统计执行时间
 def get_time(func):
     def wraper(*args, **kwargs):
         start_time = time.time()
@@ -59,6 +63,7 @@ def get_time(func):
     return wraper
 
 
+# 查找，并移动文件，多个相同的文件，则更改名字移入
 @get_time
 def find_dup_files(file_dir, dup_file_dir):
     def get_duplicate():
@@ -81,6 +86,7 @@ def find_dup_files(file_dir, dup_file_dir):
                 move_files(file_rename, dup_file_dir)
 
 
+# 统计文件夹与文件个数
 def file_stat(dir_src):
     global dir_num, file_num
     for x in os.listdir(dir_src):
@@ -104,24 +110,22 @@ if __name__ == '__main__':
     # file_dir = 'E:\\test_files'
     # file_dup_path = 'E:\\duplicated_files'
     while True:
-        file_dir = input("请输入文件路径： ")
+        file_src = input("请输入文件路径： ")
         file_dup_path = input("请输入重复文件保存路径： ")
         # if not os.path.exists(file_dup_path):
-        #     file_dup_path = file_dup_path or 'C:\\duplicate_files'
+        #     file_dup_path = file_dup_path or 'duplicate_files'
         #     os.mkdir(file_dup_path)  # for windows
-        num = file_stat(file_dir)
-        print(file_dir, "文件夹下共有：", num[0], "个文件夹和 ", num[1], "个文件")
+        num = file_stat(file_src)
+        print(file_src, "文件夹下共有：", num[0], "个文件夹和 ", num[1], "个文件")
         if input("是否开始扫描Y/N,回车继续：").upper() == 'N':
             print('2秒钟后关闭')
             time.sleep(2)
             break
         print("扫描中......请稍候")
-        find_dup_files(file_dir, file_dup_path)
+        find_dup_files(file_src, file_dup_path)
         if input("是否继续Y/N,回车继续：").upper() == 'N':
             print('3秒钟后关闭...')
             # print('3秒钟后关闭, Log 文件保存在当前路径下 dup_file_log.txt 中')
             time.sleep(3)
-            # sys.stdout = Logger('dup_file_log.txt')
-            # time.sleep(3)
             break
 
